ea7d53e006770173812d5ec55e2f779f715b47f2
Matt Andreko <mandreko@gmail.com>
2012-08-14 14:56:29 -0400

    Start of file writing

--- NexposeXmlParser/Main.cs
+++ NexposeXmlParser/Main.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Linq;
 
 namespace NexposeXmlParser
 {
@@ -14,18 +16,34 @@ namespace NexposeXmlParser
             }
 
             Parser parser = new Parser();
-            var nodes = parser.Parse(args[0]);
+            var nodes = parser.Parse(args[0]).ToList();
 
             // Debug print for now
-            foreach (var node in nodes)
+            //foreach (var node in nodes)
+            //{
+            //    Console.WriteLine(node.Host);
+            //    foreach (var endpoint in node.Ports)
+            //    {
+            //        Console.WriteLine("\t{0}:{1}", endpoint.Protocol, endpoint.PortNumber);
+            //    }
+            //}
+
+            // Get a distinct list of port numbers
+            foreach(var port in nodes.SelectMany(p => p.Ports).Select(p => p.PortNumber).Distinct())
             {
-                Console.WriteLine(node.Host);
-                foreach (var endpoint in node.Ports)
+                int modifiedClosurePort = port;
+                //TODO: Handle where to save output files
+                using (StreamWriter stream = new StreamWriter(string.Format("{0}.txt", port)))
                 {
-                    Console.WriteLine("\t{0}:{1}", endpoint.Protocol, endpoint.PortNumber);
+                    // distinct list of IPs per port
+                    foreach (var ip in nodes.Where(n => n.Ports.Any(p => p.PortNumber == modifiedClosurePort)).Distinct())
+                    {
+                        stream.WriteLine(ip.Host);
+                    }
+                    stream.Close();
                 }
             }
-
+            
             Console.WriteLine("Done");
             Console.ReadLine();
         }
